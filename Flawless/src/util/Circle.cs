866e4361d8c5c69723d2a2bcb03c9d25f4d80b10
Aeneaes <sven.timmermann@wtnet.de>
2017-04-26 20:46:14 +0200

    Added circle Class
    Class includes setter for Position (x and y simultaniously) and a
    intersects with method to determine collision

---
+++ Flawless/src/util/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Flawless.util
+{
+    class Circle
+    {
+        public float x { get; protected set; }
+        public float y { get; protected set; }
+        public float radius { get; protected set; }
+
+        public Circle() { }
+
+        public Circle(float x, float y, float radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public void setPosition(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool intersectsWith(Circle c)
+        {
+            float distancePow2 = (c.x - this.x) * (c.x - this.x) + (c.y - this.y) * (c.y - this.y);
+            return distancePow2 < (c.radius + radius) * (c.radius + radius);                        /* since distance ^2 is used, the 
+                                                                                                       (radius + radius) is squared too*/
+        }
+    }
+
+
+
+}
