acf1399e4ae3f289a8972c50c7005de847bdd186
kiessler <jonas.kiessler@st.ovgu.de>
2017-05-03 22:58:37 +0200

    Added Enemy Interface

---
+++ Flawless/src/actors/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+
+namespace _Flawless.src.actors
+{
+    interface Enemy
+    {
+        Vector2f GetPosition();
+        void SetPosition(float x, float y);
+        void SetStartTime(float time);
+        void KillEnemy();
+        void ResetEnemy();
+        void TakeControl();        
+    }
+}
