c0f2025a61bc1cf5841b233a8ba77ca926d9a004
kiessler <jonas.kiessler@st.ovgu.de>
2017-05-03 23:16:28 +0200

    Added Interface Shenanigans

--- Flawless/src/actors/enemies/EnemyB.cs
+++ Flawless/src/actors/enemies/EnemyB.cs
@@ -3,12 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
 
 namespace _Flawless.actors.enemies
 {
-    class EnemyB : IEnemy
+    class EnemyB : IEnemy, IActable
     {
-        public EnemyB()
-        {}
+        private Vector2f position;
+        private Sprite texture;
+        public EnemyB() { }
+
+        public void Draw(RenderWindow _window)
+        {
+            _window.Clear(new Color(100, 100, 100));
+            _window.Draw(texture);
+        }
+
+        public Vector2f GetPosition() { return position; }
+
+        public bool IsExpired()
+        {
+            return false;
+        }
+
+        public void KillEnemy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetEnemy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPosition(float x, float y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPosition(int x, int y) { position = new Vector2f(x, y); }
+
+        public void SetStartTime(float time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float StartTime()
+        {
+            return 0f;
+        }
+
+        public void TakeControl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(float _deltaTime)
+        {
+
+        }
     }
 }
