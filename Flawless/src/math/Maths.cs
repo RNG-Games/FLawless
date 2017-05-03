718d00c8101cb537384fd1a7687b381060aa4bbe
Aeneaes <sven.timmermann@wtnet.de>
2017-05-03 23:43:23 +0200

    Added Maths
    Includes converter toPolar and toCartesian

---
+++ Flawless/src/math/Maths.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+
+namespace _Flawless.src.math
+{
+    class Maths
+    {
+        /// <summary>
+        /// Erzeugt verwendbare Koordinaten für den Positionsvektor
+        /// </summary>
+        /// <param name="position">referenzierte Position (wird verändert)</param>
+        /// <param name="spawnPosition">Rotationszentrum</param>
+        /// <param name="angle">Winkel</param>
+        /// <param name="radius">Radius</param>
+        public static Vector2f toCartesian( Vector2f spawnPosition, double angle, double radius)
+        {
+            angle = ((angle * Math.PI) / 180);
+            float X = Convert.ToSingle(radius * Math.Cos(angle)) + spawnPosition.X;
+            float Y = Convert.ToSingle(radius * Math.Sin(angle)) + spawnPosition.Y;
+            return new Vector2f(X, Y);
+        }
+
+        public static Vector2f toPolar ( Vector2f spawnPosition, Vector2f position)
+        {
+            float radius = (float) Math.Sqrt(Math.Pow(position.X - spawnPosition.X, 2) + Math.Pow(position.Y - spawnPosition.Y, 2));
+            float angle = (float) Math.Acos(position.X - spawnPosition.X / radius);
+            if ( position.Y < spawnPosition.Y) { angle = 360 - angle; }    //if the positon is below the spwan, the angle is "spun" around the x axis
+            return new Vector2f(radius, angle);
+        }
+    }
+}
+
