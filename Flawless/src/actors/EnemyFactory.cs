4f30857b2c9e52846c567a20e8b7b8da95099139
Desperate-kun <nico.fischer98@web.de>
2017-05-03 23:01:21 +0200

    Add Enemy Classes

---
+++ Flawless/src/actors/EnemyFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Flawless.src.actors
+{
+    class EnemyFactory
+    {
+        public Enemy GetEnemy(String enemyType)
+        {
+            if(enemyType == null)
+            {
+                return null;
+            }
+            if(enemyType == "A")
+            {
+                return new EnemyA();
+            }
+            if(enemyType == "B")
+            {
+                return new EnemyB();
+            }
+            if(enemyType == "C")
+            {
+                return new EnemyC();
+            }
+        }
+    }
+}
---
+++ Flawless/src/actors/enemies/EnemyA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Flawless.src.actors.enemies
+{
+    class EnemyA : Enemy
+    {
+        public EnemyA()
+        {}
+    }
+}
