d24e105d7504a92069631eff5e7cd4861e1c5697
Mao <jonas.kiessler@st.ovgu.de>
2017-05-10 21:53:20 +0200

    What I did at university

--- Flawless/src/actors/BulletFactory.cs
+++ Flawless/src/actors/BulletFactory.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SFML.System;
+using _Flawless.actors.bullets;
 
 namespace _Flawless.actors
 {
--- Flawless/src/actors/patterns/PatternA.cs
+++ Flawless/src/actors/patterns/PatternA.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.System;
 
 namespace _Flawless.actors.patterns
 {
-    class PolarPatterns : Pattern
+    abstract class PolarPattern : Pattern
     {
-        List<Bullet> bulletList;
-        BulletFactory factory = new BulletFactory();
+        protected int bulletNum;
+        protected Bullet.BulletType type;
+        protected List<Bullet> bulletList;
+        protected BulletFactory factory = new BulletFactory();
+        protected int angle;
+        protected Vector2f position;
 
-        public override void Update(float _deltaTime)
+        public PolarPattern(int bulletNum, Bullet.BulletType type, Vector2f position)
         {
-            
+
         }
     }
 
-    class PPBurst
+    class PPBurst : PolarPattern
     {
-        int bulletNum_;
-        public PPBurst(int bulletNum_) {
-            this.bulletNum_ = bulletNum_;
+        int angleChange;
+
+        public PPBurst(int bulletNum, Bullet.BulletType type, Vector2f position, ) {
+            this.bulletNum = bulletNum;
+            this.type = type;
+        }
+
+        public override void Update(float _deltaTime)
+        {
+            for (int i = 0; i < bulletNum; i++)
+            {
+                bulletList.Add(factory.GetBullet(type, position, angle));
+                angle += angleChange;
+            }
 
         }
+
 
     }
 }
