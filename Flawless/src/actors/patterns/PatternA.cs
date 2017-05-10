67f096aafad83b705107087802bdc57dc8a99c7b
kiessler <jonas.kiessler@st.ovgu.de>
2017-05-10 23:22:10 +0200

    :smile: :point_left: :cat: :cat: :cat: :cat: :cat: :cat: (added interval PPs+ additional fixes on Burst PPs)

--- Flawless/src/actors/patterns/PatternA.cs
+++ Flawless/src/actors/patterns/PatternA.cs
@@ -15,34 +15,72 @@ namespace _Flawless.actors.patterns
         protected BulletFactory factory = new BulletFactory();
         protected float angle;
         protected Vector2f position;
+        protected float angleChange;
 
-        public PolarPattern(int bulletNum, Bullet.BulletType type, Vector2f position, float angle)
+        public PolarPattern(int bulletNum, Bullet.BulletType type, Vector2f position, float angle, float angleChange)
         {
             this.bulletNum = bulletNum;
             this.type = type;
             this.position = position;
+            this.angle = angle;
+            this.position = position;
+            this.angleChange = angleChange;
+        }
+
+        protected virtual void updateBullets(float _deltaTime)
+        {
+            foreach (var current in bulletList)
+            {
+                current.Update(_deltaTime);
+            }
         }
     }
 
+    /* Constant Polar Pattern */
+
     class PPBurst : PolarPattern
     {
-        int angleChange;
-
-        public PPBurst(int bulletNum, Bullet.BulletType type, Vector2f position) {
-            this.bulletNum = bulletNum;
-            this.type = type;
-        }
-
-        public override void Update(float _deltaTime)
+        public PPBurst(int bulletNum, Bullet.BulletType type, Vector2f position, float angle, float angleChange) 
+            : base(bulletNum, type, position, angle, angleChange)
         {
             for (int i = 0; i < bulletNum; i++)
             {
                 bulletList.Add(factory.GetBullet(type, position, angle));
-                angle += angleChange;
             }
-
         }
 
+        public override void Update(float _deltaTime)
+        {
+            updateBullets(_deltaTime);
+        }
+    }
+
+    /* Interval Polar Pattern */
+    class PPInterval : PolarPattern
+    {
+        private bool isSpawned = false;
+        private float timeCounter = 0;
+        private float interval;
+
+        public PPInterval(int bulletNum, Bullet.BulletType type, Vector2f position, float angle, float angleChange, float interval)
+            : base(bulletNum, type, position, angle, angleChange)
+        {
+            this.interval = interval;
+        }
+
+        public override void Update(float _deltaTime)
+        {
+            if (!isSpawned && (timeCounter >= interval))
+            {
+                for (int i = 0; i < bulletNum; i++)
+                {
+                    bulletList.Add(factory.GetBullet(type, position, angle));
+                }
+            }
+
+            updateBullets(_deltaTime);
+            timeCounter += _deltaTime;
+        }
 
     }
 }
