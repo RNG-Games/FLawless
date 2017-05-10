fb55e9f9378c32073a1a29d28e6e481a662d5e4f
Desperate-kun <nico.fischer98@web.de>
2017-05-10 23:29:14 +0200

    Updated LinearBullet

--- Flawless/src/actors/bullets/LinearBullet.cs
+++ Flawless/src/actors/bullets/LinearBullet.cs
@@ -5,23 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using SFML.System;
 using SFML.Graphics;
+using _Flawless.math;
 
 namespace _Flawless.actors.bullets
 {
     class LinearBullet : Bullet
     {
-        Vector2f direction;
+        Vector2f movement;
 
         public LinearBullet(Vector2f _position, float _angle) : base(_position, _angle)
         {
             texture = new Sprite(Resources.GetTexture("player.png")) { Position = position };
-            speed = 0.1f;
-            direction = 
+            speed = 40f;
         }
 
         public override void Update(float _deltaTime)
         {
             base.Update(_deltaTime);
+            Vector2f coord = Maths.toPolar(spawnPosition, position);
+            coord.X += speed * _deltaTime;
+            texture.Position = position;
         }
     }
 }
