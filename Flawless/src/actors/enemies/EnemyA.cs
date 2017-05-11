bb331e0938125093f1753b1d6c254274427d3adf
kiessler <jonas.kiessler@st.ovgu.de>
2017-05-11 21:40:09 +0200

    More fixes on Enemy

--- Flawless/src/actors/enemies/EnemyA.cs
+++ Flawless/src/actors/enemies/EnemyA.cs
@@ -11,14 +11,18 @@ namespace _Flawless.actors.enemies
 {
     class EnemyA : Enemy
     {
-
+        bool isSpawned = false;
         public EnemyA(Vector2f position) : base(position) {
             texture = new Sprite(Resources.GetTexture("player.png")) { Position = position };
-            PolarPattern test = new PPBurst(10, Bullet.BulletType.A, position, new math.Angle(0f), new math.Angle(1f));
         }
 
         public override void Update(float _deltaTime)
         {
+            if (isSpawned == false) {
+                PolarPattern test = new PPBurst(10, Bullet.BulletType.A, position, new math.Angle(0f), new math.Angle(1f));
+                isSpawned = true;
+            }
+
             base.Update(_deltaTime);
             if (frameCounter % 10000 < 5000)
             {
