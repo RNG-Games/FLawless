66321902074a8cce143779ce6d949cbfe1f97fbe
Aeneaes <sven.timmermann@wtnet.de>
2017-05-11 22:53:21 +0200

    retryed prev change -.-
    me stooopid

--- Flawless/src/actors/TextBox.cs
+++ Flawless/src/actors/TextBox.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.System;
+using SFML.Graphics;
 
-namespace _Flawless.src.actors
+namespace _Flawless.actors
 {
-    class textBox
+    abstract class TextBox : IActable
     {
+        protected Text text; //full text
+        protected String currentDisplayed; 
+        protected Sprite texture;
+        protected Vector2f position;
+        protected Font font; // maybe redundant, seemed practical to have here for the unprofessional eye of mine tho
+
+        public TextBox (Vector2f _position)
+        {
+            position = _position;
+        }
+
+        public void Draw(RenderWindow _window)
+        {
+            _window.Draw(texture);
+            _window.Draw(text);
+        }
+
+        public bool IsExpired()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float StartTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Update(float _deltaTime)
+        {
+            text.DisplayedString = currentDisplayed;
+
+        }
     }
 }
