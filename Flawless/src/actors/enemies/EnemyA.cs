e39463e7f3252367e4f41d9ac049a0932d5e69ef
kiessler <jonas.kiessler@st.ovgu.de>
2017-05-11 21:32:44 +0200

    Test implementation of pattern

--- Flawless/src/actors/enemies/EnemyA.cs
+++ Flawless/src/actors/enemies/EnemyA.cs
@@ -5,13 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using SFML.Graphics;
 using SFML.System;
+using _Flawless.actors.patterns;
 
 namespace _Flawless.actors.enemies
 {
     class EnemyA : Enemy
     {
+
         public EnemyA(Vector2f position) : base(position) {
             texture = new Sprite(Resources.GetTexture("player.png")) { Position = position };
+            PolarPattern test = new PPBurst(10, Bullet.BulletType.A, position, new math.Angle(0f), new math.Angle(1f));
         }
 
         public override void Update(float _deltaTime)
