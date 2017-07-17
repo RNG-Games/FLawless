f78f380c2c55ae54f0fd49efe0ed1c3be32f4192
Michael Mario Kubicki <Kubicki-Michael@live.de>
2017-07-17 16:38:03 +0200

    Fix
    I should test Stuff before commiting it

--- Flawless/src/Program.cs
+++ Flawless/src/Program.cs
@@ -86,7 +86,7 @@ namespace _Flawless
             _deltaTime = _clock.ElapsedTime.AsSeconds();
             _gameTime += _deltaTime;
             _clock.Restart();
-			_text.DisplayedString =$"{_gameTime/60f:f2} min";
+			_text.DisplayedString =$"{_gameTime:f2} s";
             _current = States.Peek();
             _current.Update(_deltaTime);
         }
